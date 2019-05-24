28110320e34322243daddf99052beceb22eac974
Misaki Koonce <misaki.koonce@gmail.com>
2019-05-23 22:54:30 -0700

    Add AddNewItem component.

---
+++ src/AddNewItem.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+
+class AddNewItem extends Component {
+    state = {
+        value: '',
+    };
+
+    // setState
+    handleChange = event => {
+        this.setState({ value: event.target.value });
+      };
+
+    addItem = event => {
+        event.preventDefault();
+        this.props.onAddItem(this.state.value);
+      };
+
+    inputIsEmpty = () => {
+    return this.state.value === '';
+    };
+
+    render () {
+        return (
+            <div>
+                <form onSubmit={this.addItem}>
+                    <input
+                        type="text"
+                        placeholder="Enter New Item"
+                        value={this.state.value}
+                        onChange={this.handleChange}
+                    />
+                    <button disabled={this.inputIsEmpty()}>Add</button>
+                </form>
+            </div>
+        )
+    };
+}
+
+export default AddNewItem
